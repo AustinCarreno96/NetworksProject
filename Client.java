14b13f2ef642aa55d013cf661b3cdc29d6b22baa
Austin <austinraycarreno96@gmail.com>
2021-10-24 22:36:30 -0400

    Rebuilt program

--- Client.java
+++ Client.java
@@ -3,216 +3,89 @@ import java.net.*;
 import java.util.*;
 import java.lang.*;
 
+public class Client extends Thread{
 
-public class Client implements Runnable {
+    private static int portNumber = 3001;
+    public String command;
 
-    // TODO: TEST
-    private static final int SERVER_PORT = 312;
-
-    static String command = null;
-    int clients;
-
-    // TODO: Need command line arguments for IP Address and Port Number
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-
-
-        // a queue to hold threads and remove them as called
-        Queue<Thread> clientQueue = new LinkedList<Thread>();
-
-        while(command != "7") {
-
-            // menu for the user
-            System.out.println("Choose from menu options");
-            System.out.println("1. Date and time");
-            System.out.println("2. Uptime");
-            System.out.println("3. Memory use");
-            System.out.println("4. Netstat");
-            System.out.println("5. Current Users");
-            System.out.println("6. Running Processes");
-            System.out.println("7. Exit");
-            System.out.print("> ");
-            command = scanner.nextLine();
-
-            // TODO: Need to check user input for correct commands
-
-            // If user chooses 7, quit program
-            if (command.equals("7")) {
-                break;
-            }
-
-            // how many threads will be needed
-            System.out.println("\nHow many requests would you like to run? (Please select from 1, 5, 10, 15, 20, or 25)");
-            int requestAmount = scanner.nextInt();
-
-            // test for a correct amount of threads
-            while (requestAmount != 1 && requestAmount != 5 && requestAmount != 10 && requestAmount != 15 && requestAmount != 20 && requestAmount != 25) {
-                System.out.println("\nInvalid entry, please enter either 1, 5, 10, 15, 20, or 25:");
-                requestAmount = scanner.nextInt();
-            }
-
-
-            // filling queue with threads
-            for (int i = 0; i < requestAmount; i++) {
-                // TODO: Come back when client class is made
-                clientQueue.add(new Thread(new Client(i)));
-            }
-
-            // Calling the Threads from the queue
-            for (int j = 0; j < requestAmount; j++) {
-                clientQueue.poll().start();
-            }
-        }// end of while loop
-
-        // Grab the server IP Address
-        // TODO: Wont be necessary, need command line arguments
-//        String SERVER_IP = scanner.next();
-
-//        try {
-//            // Create socket with server IP Address and port number
-//            // TODO: Test, need to get rid of host IP for later
-//            Socket socket = new Socket("192.168.1.84", SERVER_PORT);
-//
-//            DataInputStream input2 = new DataInputStream(System.in);
-//            DataOutputStream out2 = new DataOutputStream(socket.getOutputStream());
-//
-//            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-//
-//            // Pulling the menu from the server
-//            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-//
-//            String serverResponse;
-//
-//
-//
-//            // Testing if server and client are connected
-////            while ((serverResponse = input.readLine()) != null) {
-////                System.out.println(serverResponse);
-////                if (serverResponse.contains("*")) {
-////                    break;
-////                }
-////            }
-//
-//            System.out.println("TESTTESTTEST");
-////            System.out.print("Enter the Hostname: ");
-//            Scanner userInput = new Scanner(System.in);
-//            String userChoice = userInput.next();
-//            Thread testThread = new Thread(userChoice);
-//            testThread.start();
-//            System.out.println(testThread.getName());
-//
-//            while(true) {
-//                serverResponse = input.readLine();
-////            for(int i = 0; i < 25; i++) {
-////                Scanner userInput = new Scanner(System.in);
-////                Thread testThread = new Thread(userInput.next());
-////                testThread.start();
-////                System.out.println(testThread.getName());
-////            }
-//                System.out.println("Working");
-//
-//                // Only for menu, probably needs to go
-////                while ((serverResponse = input.readLine()) != null) {
-////                    System.out.println(serverResponse);
-////                    if (serverResponse.contains("*")) {
-////                        break;
-////                    }
-////                }
-//
-//
-//
-//                if (serverResponse == null)
-//                    break;
-//
-//            }
-//
-//            socket.close();
-//            System.exit(0);
-//        } catch(SocketException se) {
-//            System.out.println("Not working");
-//            System.exit(1);
-//        }
-
-    }// end main
-
-    public Client(int value) {
-        clients = value;
+    // Constructor
+    public Client(String command) {
+        this.command = command;
     }
 
-    // Runs as many times as the user asks for
-    public void run() {
-        try {
-            // start time for client request
-            long startTime = System.currentTimeMillis();
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int amountOfThreads = 0;
+        Queue<Thread> threadQueue = new LinkedList<Thread>();
 
-            // Create socket with server IP Address and port number
-            // TODO: Test, need to get rid of host IP for later
-            // TODO: You need to type in your own IP Address
-            Socket socket = new Socket("192.168.1.84", SERVER_PORT);
+        // menu for the user
+        System.out.println("Choose from menu options");
+        System.out.println("1. Date and time");
+        System.out.println("2. Uptime");
+        System.out.println("3. Memory use");
+        System.out.println("4. Netstat");
+        System.out.println("5. Current Users");
+        System.out.println("6. Running Processes");
+        System.out.println("7. Exit");
+        System.out.print("> ");
+        String command = userInput.nextLine();
 
-            // Pulling the menu from the server
-            PrintWriter sent = new PrintWriter(socket.getOutputStream());
-            sent.println(command);
-            sent.flush();
+        // TODO: CREATING THREADS
+        System.out.print("Enter in the amount of times you'd like to run this command: ");
+        amountOfThreads = userInput.nextInt();
 
-            DataInputStream input2 = new DataInputStream(System.in);
-            DataOutputStream out2 = new DataOutputStream(socket.getOutputStream());
-
-            BufferedReader serverRead = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-
-
-
-            String serverResponse = "";
-            System.out.println(serverResponse);
-
-
-
-            // Testing if server and client are connected
-//            while ((serverResponse = input.readLine()) != null) {
-//                System.out.println(serverResponse);
-//                if (serverResponse.contains("*")) {
+        // TODO: COME BACK TO LATER
+//            while(amountOfThreads > 25 && amountOfThreads % 5 != 0) {
+//                if(amountOfThreads == 1) {
 //                    break;
 //                }
+//                System.out.print("Invalid Entry, Please Try Again: ");
+//                amountOfThreads = userInput.nextInt();
 //            }
 
-            System.out.println("TESTTESTTEST");
-//            System.out.print("Enter the Hostname: ");
-            Scanner userInput = new Scanner(System.in);
-            String userChoice = userInput.next();
-            Thread testThread = new Thread(userChoice);
-            testThread.start();
-            System.out.println(testThread.getName());
 
-            while(true) {
-                serverResponse = serverRead.readLine();
+        // Threads being created
+        for(int i = 0; i < amountOfThreads; i++) {
+            Client newClient = new Client(command);
+            threadQueue.add(newClient);
+        }
 
-                System.out.println("Working");
+        for(int j = 0; j < amountOfThreads; j++) {
+            threadQueue.poll().start();
+        }
+    }// end main
 
-                // Only for menu, probably needs to go
-//                while ((serverResponse = input.readLine()) != null) {
-//                    System.out.println(serverResponse);
-//                    if (serverResponse.contains("*")) {
-//                        break;
-//                    }
-//                }
+    public void run() {
+        long time = 0;
+        // TODO: TEST
+        System.out.println("Threads are Working");
+        StringBuilder builtString = new StringBuilder();
 
-                if (serverResponse == null)
-                    break;
+        try(Socket sock = new Socket("192.168.1.84", portNumber)) {
+            long start = System.currentTimeMillis();
+            System.out.println("Connected");
 
+            PrintWriter send = new PrintWriter(sock.getOutputStream());
+            send.println(command);
+            send.flush();
+
+            InputStream input = sock.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            String serverMessage;
+
+            while((serverMessage = reader.readLine()) != null) {
+                builtString.append(serverMessage + "\n");
             }
+            // TODO: LINES 21 - 23 ARE TESTS
+            String currentTime = reader.readLine();
 
-            socket.close();
-            System.exit(0);
-        } catch(SocketException se) {
-            System.out.println("Not working");
-            System.exit(1);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+            System.out.println(currentTime);
+
+        } catch(UnknownHostException UHE) {
+            System.out.println("Server cannot be found at this time");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-}// end Client class
+}
--- Server.java
+++ Server.java
@@ -1,92 +1,265 @@
 import java.io.*;
 import java.net.*;
 import java.util.Date;
+import java.util.Scanner;
 
-public class Server implements Runnable{
-    private Socket sock;
-    private static PrintWriter out;
 
-    public static void exec(String cmd) throws IOException{
-        Process p = new ProcessBuilder(cmd).start();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String connect = bf.readLine();
+public class Server {
 
-        while (connect != null){
-            out.println(connect);
-            connect = bf.readLine();
+    private static int portNumber = 3001;
+
+    // TODO: Look into making a single thread for the server that updates. Need to make switch statement and that's it
+    public static void main(String[] args) {
+        String message = "";
+
+        // Creation of a server socket
+        try(ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("Socket successfully created at port " + portNumber);
+
+            // calling accept to start listening for client requests
+            while(true) {
+                Socket sock = serverSocket.accept();
+                System.out.println("Listening");
+
+                // TODO: LINES 20 - 25 ARE ABOUT READING IN THE INPUT FROM THE CLIENT
+                // used to read data sent from client
+                // TODO: TEST
+                InputStream input = sock.getInputStream();
+
+                // used to read the input stream at a higher, more legible level
+                BufferedReader inputReader = new BufferedReader(new InputStreamReader(input));
+
+
+//            System.out.println(theReader);
+
+                // Sending data to the Client
+                OutputStream output = sock.getOutputStream();
+                // Wrapped in PrintWriter to send data in text format
+                PrintWriter writer = new PrintWriter(output, true);
+                String theReader = inputReader.readLine();
+
+                switch(theReader) {
+                    case "1":
+                        message = commandRequest("date");
+                        break;
+                    case "2":
+                        message = commandRequest("uptime");
+                        break;
+                    case "3":
+                        message = commandRequest("free -h");
+                        break;
+                    case "4":
+                        message = commandRequest("netstat");
+                        break;
+                    case "5":
+                        message = commandRequest("w");
+                        break;
+                    case "6":
+                        message = commandRequest("ps -aux");
+                        break;
+                    default:
+                        message = "Incorrect Entry, Please Try Again";
+
+                }
+
+                // TODO: TEST
+//                writer.println(new Date().toString());
+                writer.println(message);
+                writer.flush();
+                sock.close();
+            }
+        } catch(IOException e) {
+            System.out.println("The server did not connect correctly");
+            e.printStackTrace();
         }
-        out.println("*");
-        bf.close();
-    }
+    }// end main
 
-    private static final int port = 312;
-    public static void main(String[] args) throws IOException {
-        long start = System.currentTimeMillis();
-
-        ServerSocket listener = new ServerSocket(port);
-        System.out.println("Connecting to client");
-
-        // Listening for Client
-        Socket client = listener.accept();
-        String request;
-
-        // This is where the menu is coming from
-        out = new PrintWriter(client.getOutputStream(), true);
-
-        System.out.println("Connection established");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    private static String commandRequest(String requestedCommand) {
+        StringBuilder message = new StringBuilder();
 
         try {
-            while (true) {
-                request = in.readLine();
-                switch(request) {
-                    case "1":
-//                        exec("date");
-                        out.println("Server Test");
-                        break;
+            // starting the process for whichever command is chosen by the user
+            Process process = Runtime.getRuntime().exec(requestedCommand);
 
-                    case "2":
-                        exec("uptime");
-                        break;
+            // reading the output from the process ran
+            BufferedReader theReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String output;
 
-                    case "3":
-                        exec("free");
-                        break;
-
-                    case "4":
-                        exec("netstat");
-                        break;
-
-                    case "5":
-                        exec("who");
-                        break;
-
-                    case "6":
-                        exec("ps -e");
-                        break;
-
-                    case "7":
-                        client.close();
-                        listener.close();
-
-                    default:
-                        out.println("Invalid option, please try again: ");
-                        request = in.readLine();
-                }
-            }
-        } catch(SocketException e){
-            System.out.println("Connection terminated");
+            while((output = theReader.readLine()) != null) message.append(output + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("Client disconnected");
-        client.close();
-        listener.close();
-
+        return message.toString();
     }
+}
 
-    public static boolean isValidCommand(String command){
-        if(command == null);
-        return command.equals("1") || command.equals("2") || command.equals("3") || command.equals("4") || command.equals("5") || command.equals("6") || command.equals("7");
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    private static PrintWriter out;
+//    private static final int port = 312;
+//
+//    // Austin
+//    private Socket sock;
+//
+//    public static void exec(String cmd) throws IOException{
+//        Process p = new ProcessBuilder(cmd).start();
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//        String connect = bf.readLine();
+//
+//
+//        while (connect != null){
+//            out.println(connect);
+//            connect = bf.readLine();
+//        }
+//        out.println("*");
+//        bf.close();
+//    }
+//
+//
+//    public static void main(String[] args) throws IOException {
+//        long start = System.currentTimeMillis();
+//        Scanner clientInput = new Scanner(System.in);
+//
+//        ServerSocket listener = new ServerSocket(port);
+//        System.out.println("Connecting to client");
+//
+//        // Listening for Client
+//        Socket client = listener.accept();
+//        String request;
+//
+//        // This is where the menu is coming from
+//        out = new PrintWriter(client.getOutputStream(), true);
+//
+//        System.out.println("Connection established");
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//
+//        try {
+//            while (true) {
+//                request = in.readLine();
+//                switch(request) {
+//                    case "1":
+////                        exec("date");
+//                        out.println("Server Test");
+//                        break;
+//
+//                    case "2":
+//                        exec("uptime");
+//                        break;
+//
+//                    case "3":
+//                        exec("free");
+//                        break;
+//
+//                    case "4":
+//                        exec("netstat");
+//                        break;
+//
+//                    case "5":
+//                        exec("who");
+//                        break;
+//
+//                    case "6":
+//                        exec("ps -e");
+//                        break;
+//
+//                    case "7":
+//                        client.close();
+//                        listener.close();
+//
+//                    default:
+//                        out.println("Invalid option, please try again: ");
+//                        request = in.readLine();
+//                }
+//            }
+//        } catch(SocketException e){
+//            System.out.println("Connection terminated");
+//        }
+//
+//        System.out.println("Client disconnected");
+//        client.close();
+//        listener.close();
+//
+//    }
+//
+//    public static boolean isValidCommand(String command){
+//        if(command == null);
+//        return command.equals("1") || command.equals("2") || command.equals("3") || command.equals("4") || command.equals("5") || command.equals("6") || command.equals("7");
+//    }
 }
